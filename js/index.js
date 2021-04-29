5cc785f51543179e0f33a6472ee6a1752e8ce4d4
muhammadreza <mra.akhgari@gmail.com>
2021-04-29 04:55:36 +0430

    Add search bar

--- js/index.js
+++ js/index.js
@@ -2,9 +2,10 @@ const avatar = document.querySelector(".avatar_container");
 const hoverIcon = document.querySelector(".hover");
 const fullscreen = document.querySelector(".fullscreen");
 const closeButton = document.querySelector('.close');
+const usernameInput = document.querySelector('.search-div__input')
+
 
 function show() {
-    console.log("aaa");
     hoverIcon.classList.add('active');
     fullscreen.classList.add('show');
 }
@@ -14,5 +15,18 @@ function hide() {
     fullscreen.classList.remove('show');
 }
 
+function focusOnInputbar() {
+    let parent = this.parentNode.parentNode;
+    parent.classList.add('focus');
+}
+
+function blurOnInputbar() {
+    let parent = this.parentNode.parentNode;
+    if (this.value == "")
+        parent.classList.remove('focus');
+}
+
 avatar.addEventListener('click', show);
 closeButton.addEventListener('click', hide);
+usernameInput.addEventListener('focus', focusOnInputbar);
+usernameInput.addEventListener('blur', blurOnInputbar);
